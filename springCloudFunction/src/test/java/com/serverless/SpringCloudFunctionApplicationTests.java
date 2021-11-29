70ef15e703079b26a3de41544267177eb51b4b50
shubhgar <shubhgar@LIN51017713.corp.capgemini.com>
2021-11-29 17:34:55 +0530

    aws practice

---
+++ springCloudFunction/src/test/java/com/serverless/SpringCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverless;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class  SpringCloudFunctionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
